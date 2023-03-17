305ee0e5f11bd0342f0648010973b719bf7bf6c0
changsongyang <y_ch_s@qq.com>
2023-03-17 12:17:42 +0800

    Update ci-before.sh

--- .github/scripts/ci-before.sh
+++ .github/scripts/ci-before.sh
@@ -8,12 +8,12 @@ mkdir -p ${HOME}/sync
   cp imgsync ${HOME}/sync/
 } || {
   # 出现bug的时候直接使用docker的二进制文件，而不用提交代码触发ci
-  docker run --rm -tid --name tool changsongyang/google_containers_sync top
+  docker run --rm -tid --name tool ycstech/google_containers_sync top
   docker cp tool:/imgsync ${HOME}/sync/
   docker kill tool
 }
 
-docker run --rm -tid --name db changsongyang/google_containers_db2 top
+docker run --rm -tid --name db ycstech/google_containers_db2 top
 docker cp db:/bolt.db ${HOME}/sync/
 
 docker kill db
